e9b8c2e6bbe103c03d21f386c0f3cac45001a4bf
Andrew MacIsaac <amacisaac95@gmail.com>
2023-01-11 23:04:07 -0700

    Project complete

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random()*1000)+1;
+let num3 = 19;
+let num4 = 5;
+let mod = num3 % num4;
+let max = Math.max(1,3,4,5,6,7,9,11,14,16,18,19,20);
